58d005a5c2501e92891b881536fed228c08ec938
kiraqwq <lzzbangbangda@163.com>
2023-02-11 09:43:11 +0800

    项目初始化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
+// npm第三方库
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 样式
+
+// 项目中定义的组件
+import { App } from './App';
+
+// 创建项目要渲染到的根节点
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 渲染根节点
+root.render(<App />);
